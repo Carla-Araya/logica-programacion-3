abdaa35d470e50f219d05315d55261b6623e64b2
Carla-Araya <carla.araya@genstudents.org>
2024-05-17 17:18:44 -0400

    tarea terminada

---
+++ logica.js
@@ -0,0 +1,16 @@
+function calculateFactorial(){
+    let input = document.getElementById('numberInput').value;
+    let number = parseInt(input);
+
+    document.getElementById('result').innerHTML = '';
+
+    if (isNaN(number) || number < 0) {
+        alert("Please enter a valid positive number.");
+        return;
+    }
+    let factorial = 1;
+    for (let i = 1; i <= number; i++){
+        factorial *= i;
+    }
+    document.getElementById('result').innerHTML = `The factorial of ${number} is ${factorial}.`;
+}
